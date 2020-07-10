6d4dc3a81db635462380c0220f3e983a9c1e6fe9
blackbox114 <chen.junfei@nexuslink.cn>
2020-07-10 12:16:53 +0800

    Update single.ino

--- GAMEtest/single/single.ino
+++ GAMEtest/single/single.ino
@@ -29,6 +29,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void drawCourt();
 void splash();
 void printScreen();
+void SinglePlayer();
 
 uint8_t ball_x = 64, ball_y = 32;
 uint8_t ball_x0 = 64, ball_y0 = 32;
@@ -44,6 +45,7 @@ uint8_t player_y = 16;
 void setup() {
   delay(100);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);//使用I2C地址0x3C初始化（对于128x64）
+  display.clearDisplay();
   unsigned long start = millis();
   pinMode(UP_BUTTON, INPUT_PULLUP);
   pinMode(DOWN_BUTTON, INPUT_PULLUP);
@@ -62,110 +64,9 @@ void setup() {
 }
 
 void loop() {
-  drawCourt();
-  bool update = false;
-  unsigned long time = millis();
 
-  static bool up_state = false;
-  static bool down_state = false;
+  SinglePlayer();
 
-  up_state |= (digitalRead(UP_BUTTON) == LOW);
-  down_state |= (digitalRead(DOWN_BUTTON) == LOW);
-
-  if (time > ball_update) {
-    uint8_t new_x = ball_x + ball_dir_x;
-    uint8_t new_y = ball_y + ball_dir_y;
-
-    // 如果球撞到电脑的墙
-    if (new_x == 0) {
-      display.clearDisplay();
-      scoreUSER++;
-      printScores();
-      delay(100);
-      new_x = ball_x0 + ball_dir_x;
-      new_y = ball_y0 + ball_dir_y;
-
-    }
-
-    // 如果球撞到玩家的墙
-    if (new_x == 127) {
-      display.clearDisplay();
-      scoreCPU++;
-      printScores();
-      delay(100);
-      new_x = ball_x0 + ball_dir_x;
-      new_y = ball_y0 + ball_dir_y;
-
-
-    }
-
-    // 如果球撞到水平的墙
-    if (new_y == 0 || new_y == 63) {
-      ball_dir_y = -ball_dir_y;
-      new_y += ball_dir_y + ball_dir_y;
-
-
-    }
-
-    //如果球撞到电脑的拍子上
-    if (new_x == CPU_X && new_y >= cpu_y && new_y <= cpu_y + PADDLE_HEIGHT) {
-      ball_dir_x = -ball_dir_x;
-      new_x += ball_dir_x + ball_dir_x;
-    }
-
-    // 如果球撞到玩家的拍子上
-    if (new_x == PLAYER_X
-        && new_y >= player_y
-        && new_y <= player_y + PADDLE_HEIGHT)
-    {
-      ball_dir_x = -ball_dir_x;
-      new_x += ball_dir_x + ball_dir_x;
-    }
-
-    display.drawPixel(ball_x, ball_y, BLACK);
-    display.drawPixel(new_x, new_y, WHITE);
-    ball_x = new_x;
-    ball_y = new_y;
-
-    ball_update += BALL_RATE;
-
-    update = true;
-  }
-
-  if (time > paddle_update) {
-    paddle_update += PADDLE_RATE;
-
-    // CPU paddle
-    display.drawFastVLine(CPU_X, cpu_y, PADDLE_HEIGHT, BLACK);
-    const uint8_t half_paddle = PADDLE_HEIGHT >> 1;
-    if (cpu_y + half_paddle > ball_y) {
-      cpu_y -= 1;
-    }
-    if (cpu_y + half_paddle < ball_y) {
-      cpu_y += 1;
-    }
-    if (cpu_y < 1) cpu_y = 1;
-    if (cpu_y + PADDLE_HEIGHT > 63) cpu_y = 63 - PADDLE_HEIGHT;
-    display.drawFastVLine(CPU_X, cpu_y, PADDLE_HEIGHT, WHITE);
-
-    // Player paddle
-    display.drawFastVLine(PLAYER_X, player_y, PADDLE_HEIGHT, BLACK);
-    if (up_state) {
-      player_y -= 1;
-    }
-    if (down_state) {
-      player_y += 1;
-    }
-    up_state = down_state = false;
-    if (player_y < 1) player_y = 1;
-    if (player_y + PADDLE_HEIGHT > 63) player_y = 63 - PADDLE_HEIGHT;
-    display.drawFastVLine(PLAYER_X, player_y, PADDLE_HEIGHT, WHITE);
-
-    update = true;
-  }
-
-  if (update)
-    display.display();
 }
 
 
@@ -228,4 +129,122 @@ void printScores() {
   display.print(scoreUSER);
 
   display.display();
+}
+
+
+
+// void MultiPlayer(){}
+
+void SinglePlayer() {
+  while(1){
+
+    bool update = false;
+    unsigned long time = millis();
+
+    static bool up_state = false;
+    static bool down_state = false;
+
+    up_state |= (digitalRead(UP_BUTTON) == LOW);
+    down_state |= (digitalRead(DOWN_BUTTON) == LOW);
+
+    if (time > ball_update) {
+      uint8_t new_x = ball_x + ball_dir_x;
+      uint8_t new_y = ball_y + ball_dir_y;
+
+    // 如果球撞到电脑的墙
+      if (new_x == 0) {
+        display.clearDisplay();
+        drawCourt();
+        scoreUSER++;
+        printScores();
+        delay(1000);
+        new_x = ball_x0 + ball_dir_x;
+        new_y = ball_y0 + ball_dir_y;
+
+      }
+
+    // 如果球撞到玩家的墙
+      if (new_x == 127) {
+        display.clearDisplay();
+        drawCourt();
+        scoreCPU++;
+        printScores();
+        delay(1000);
+        new_x = ball_x0 + ball_dir_x;
+        new_y = ball_y0 + ball_dir_y;
+
+
+      }
+
+    // 如果球撞到水平的墙
+      if (new_y == 0 || new_y == 63) {
+        ball_dir_y = -ball_dir_y;
+        new_y += ball_dir_y + ball_dir_y;
+
+
+      }
+
+    //如果球撞到电脑的拍子上
+      if (new_x == CPU_X && new_y >= cpu_y && new_y <= cpu_y + PADDLE_HEIGHT) {
+        ball_dir_x = -ball_dir_x;
+        new_x += ball_dir_x + ball_dir_x;
+      }
+
+    // 如果球撞到玩家的拍子上
+      if (new_x == PLAYER_X
+        && new_y >= player_y
+        && new_y <= player_y + PADDLE_HEIGHT)
+      {
+        ball_dir_x = -ball_dir_x;
+        new_x += ball_dir_x + ball_dir_x;
+      }
+
+      display.drawPixel(ball_x, ball_y, BLACK);
+      display.drawPixel(new_x, new_y, WHITE);
+      ball_x = new_x;
+      ball_y = new_y;
+
+      ball_update += BALL_RATE;
+
+      update = true;
+    }
+
+    if (time > paddle_update) {
+      paddle_update += PADDLE_RATE;
+
+    // CPU paddle
+      display.drawFastVLine(CPU_X, cpu_y, PADDLE_HEIGHT, BLACK);
+      const uint8_t half_paddle = PADDLE_HEIGHT >> 1;
+      if (cpu_y + half_paddle > ball_y) {
+        cpu_y -= 1;
+      }
+      if (cpu_y + half_paddle < ball_y) {
+        cpu_y += 1;
+      }
+      if (cpu_y < 1) cpu_y = 1;
+      if (cpu_y + PADDLE_HEIGHT > 63) cpu_y = 63 - PADDLE_HEIGHT;
+      display.drawFastVLine(CPU_X, cpu_y, PADDLE_HEIGHT, WHITE);
+
+    // Player paddle
+      display.drawFastVLine(PLAYER_X, player_y, PADDLE_HEIGHT, BLACK);
+      if (up_state) {
+        player_y -= 1;
+      }
+      if (down_state) {
+        player_y += 1;
+      }
+      up_state = down_state = false;
+      if (player_y < 1) player_y = 1;
+      if (player_y + PADDLE_HEIGHT > 63) player_y = 63 - PADDLE_HEIGHT;
+      display.drawFastVLine(PLAYER_X, player_y, PADDLE_HEIGHT, WHITE);
+
+      update = true;
+    }
+
+    if (update)
+      display.display();
+
+
+  }
+
 }
